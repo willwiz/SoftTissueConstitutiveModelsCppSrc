58e9edb3e58fee57a09f1219140b6d9b26025b75
Will Zhang <willwz@gmail.com>
2023-12-15 14:58:50 -0500

    restructure

--- CTvalues_kde.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace ctv
-{
-  // KDE parameters
-  extern const double gauss_bandwitchfactor = 1.0/6.0;
-  extern const double kde_bound_trigger_value = 2.0/3.0;
-} // namespace ct_pars
-
---
+++ cpp/CTvalues_kde.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace ctv {
+// KDE parameters
+extern const double gauss_bandwitchfactor = 1.0 / 6.0;
+extern const double kde_bound_trigger_value = 2.0 / 3.0;
+} // namespace ctv
